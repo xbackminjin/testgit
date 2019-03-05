b73e47559ef0763439cece4247fbc80b48a69c60
‘xbackminjin’ <153409186@qq.com>
2019-03-05 11:56:14 +0800

    同时修改一个文件

--- src/main/java/xback/minjin/User.java
+++ src/main/java/xback/minjin/User.java
@@ -2,7 +2,7 @@ package xback.minjin;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("minjin");
         System.out.println("minjin2");
+        System.out.println("LAY");
     }
 }
